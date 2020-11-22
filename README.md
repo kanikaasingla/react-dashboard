b5253598133c7e03c778120a33f1ec113c25580a
Kanika Singla <singla.skanika@gmail.com>
2020-11-22 15:00:13 +0530

    commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# React-Dashboard-UI
+
+Dashboard developed using Reactjs and Apex Chart
+
+![UI](https://user-images.githubusercontent.com/44068102/99897140-0472b700-2cbd-11eb-9669-c670d31fda00.png)
+
+
+#### How to run
+
+```
+npm install
+npm start
+```
+
---
+++ src/Components/Charts/Area.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Chart from "react-apexcharts";
+const Area = () => {
+  const series = [
+    {
+      name: "series1",
+      data: [31, 40, 28, 51, 42, 20, 100],
+    },
+  ];
+
+  const options = {
+    dataLabels: {
+      enabled: false,
+    },
+    chart: {
+      toolbar: {
+        show: false,
+      },
+    },
+    grid: {
+      show: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      labels: {
+        show: false,
+      },
+      type: "datetime",
+      categories: [
+        "2018-09-19T00:00:00.000Z",
+        "2018-09-19T01:30:00.000Z",
+        "2018-09-19T02:30:00.000Z",
+        "2018-09-19T03:30:00.000Z",
+        "2018-09-19T04:30:00.000Z",
+        "2018-09-19T05:30:00.000Z",
+        "2018-09-19T06:30:00.000Z",
+      ],
+    },
+    tooltip: {
+      x: {
+        format: "dd/MM/yy HH:mm",
+      },
+    },
+    yaxis: {
+      labels: {
+        show: false,
+      },
+    },
+  };
+  return (
+    <div>
+      <Chart
+        options={options}
+        series={series}
+        type="area"
+        width="100%"
+        height="160"
+      />
+    </div>
+  );
+};
+
+export default Area;
